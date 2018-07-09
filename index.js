76965669f8856711865d07f5530bc238a7fe984a
Tristan Southwell <tristan.southwell@hotmail.com>
2018-07-09 21:12:32 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const store = require('./store');
+const PORT = 8080;
+
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.post('/createUser', (req, res) => {
+  store.createUser({
+    username: req.body.username,
+    password: req.body.password
+  })
+    .then(() => res.sendStatus(200))
+});
+
+app.listen(PORT, () => console.log(`App running on ${PORT}`));
